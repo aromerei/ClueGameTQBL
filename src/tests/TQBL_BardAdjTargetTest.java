0242ff0489e7bc286b927cc0391d26e152e76159
Tyler Quast <tylerquast@mymail.mines.edu>
2017-03-02 11:59:55 -0700

    test case

---
+++ src/tests/TQBL_BardAdjTargetTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TQBL_BardAdjTargetTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		lol hi ben
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
