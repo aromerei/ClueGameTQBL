239cd62dafb8c913225226c5342cb222618a65f5
Benjamin Lin <btlin@mines.edu>
2017-03-01 17:53:30 -0700

    implemented loadRoomConfig in Board class

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -1,9 +1,12 @@
 package clueGame;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
-
-
 
 public class Board {
 	private int numRows;
@@ -14,61 +17,90 @@ public class Board {
 	private Set<BoardCell> targets;
 	private String boardConfigFile;
 	private String roomConfigFile;
-	
+	private boolean debug = true;
+
 	// variable used for singleton pattern
 	private static Board theInstance = new Board();
-		// ctor is private to ensure only one can be created
+
+	// ctor is private to ensure only one can be created
 	private Board() {
 	}
-		// this method returns the only Board
+
+	// this method returns the only Board
 	public static Board getInstance() {
-			return theInstance;
+		return theInstance;
+	}
+
+	public void initialize() {
+		legend = new HashMap<Character, String>();
+		targets = new HashSet<BoardCell>();
+		try {
+			loadRoomConfig();
+			loadBoardConfig();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
-	
-	
-	public void initialize()
-	{
-		
+		calcAdjacencies();
 	}
-	
-	public void loadRoomConfig()
-	{
-		
+
+	public void loadRoomConfig() throws FileNotFoundException {
+		FileReader reader;
+		reader = new FileReader(roomConfigFile);
+		Scanner in = new Scanner(reader);
+		while (in.hasNextLine()) {
+			String entry = in.nextLine();
+			if (debug)
+				System.out.println("adding entry: " + entry);
+			legend.put(entry.charAt(0), entry.substring(3, entry.indexOf(",", 3)));
+		}
+		if (debug)
+			System.out.println("added " + legend.size() + " legend entries");
 	}
-	
-	public void loadBoardConfig()
-	{
-		
+
+	public void loadBoardConfig() throws FileNotFoundException {
+		FileReader reader;
+		reader = new FileReader(boardConfigFile);
+		Scanner in = new Scanner(reader);
+		int col = 0;
+		in.useDelimiter(",");
+		int row = 0;		
+		while (in.hasNext()) {
+			break;
+		}
 	}
-	
-	public void calcAdjacencies()
-	{
-		
+
+	public void calcAdjacencies() {
+
 	}
-	
-	public void calcTargets(BoardCell startCell, int pathLength) 
-	{
-		
+
+	public void calcTargets(BoardCell startCell, int pathLength) {
+
 	}
-	public void setConfigFiles(String string, String string2) {
-		boardConfigFile = string;
-		roomConfigFile = string2;
-		
+
+	public void setConfigFiles(String board, String rooms) {
+		boardConfigFile = board;
+		if (debug)
+			System.out.println(boardConfigFile);
+		roomConfigFile = rooms;
 	}
+
 	public Map<Character, String> getLegend() {
-	
-		return null;
+
+		return legend;
 	}
+
 	public int getNumRows() {
-		
+
 		return numRows;
 	}
+
 	public int getNumColumns() {
-	
+
 		return numColumns;
 	}
+
 	public BoardCell getCellAt(int i, int j) {
-		
+
 		return board[i][j];
 	}
 
--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -48,7 +48,7 @@ public class BoardCell {
 	}
 
 	public char getInitial() {
-		return ' ';
+		return initial;
 	}
 
 	
