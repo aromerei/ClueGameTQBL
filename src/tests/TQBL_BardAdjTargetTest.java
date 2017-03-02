3a32fcaada30ba2236494578f626a031f01c826c
Tyler Quast <tylerquast@mymail.mines.edu>
2017-03-02 12:05:38 -0700

    added test adjaencies inside room

--- src/tests/TQBL_BardAdjTargetTest.java
+++ src/tests/TQBL_BardAdjTargetTest.java
@@ -2,19 +2,46 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Set;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import clueGame.Board;
+import clueGame.BoardCell;
+
 public class TQBL_BardAdjTargetTest {
 
+	private static Board board;
+
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		lol hi ben
+	public static void setUp() {
+		// Board is singleton, get the only instance and initialize it
+		board = Board.getInstance();
+		board.initialize();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAdjacenciesInsideRooms() {//fix later
+		// Test a corner
+		Set<BoardCell> testList = board.getAdjList(0, 0);
+		assertEquals(0, testList.size());
+		// Test one that has walkway underneath
+		testList = board.getAdjList(1, 13);
+		assertEquals(0, testList.size());
+		// Test one that has walkway above
+		testList = board.getAdjList(15, 4);
+		assertEquals(0, testList.size());
+		// Test one that is in middle of room
+		testList = board.getAdjList(18, 10);
+		assertEquals(0, testList.size());
+		// Test one beside a door
+		testList = board.getAdjList(18, 17);
+		assertEquals(0, testList.size());
+		// Test one in a corner of room
+		testList = board.getAdjList(5, 20);
+		assertEquals(0, testList.size());
 	}
+	
 
 }
